2d4d1473a9d9c969fed27d3dbe4618c0490856c2
Lihiten <lihiten@post.bgu.ac.il>
2025-01-27 19:51:34 +0200

    contact signup עובדים

--- part3/pages/signup/signup.py
+++ part3/pages/signup/signup.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template
 
-signup_bp = Blueprint('signup'
-                      , __name__,
-                      template_folder='templates')
+signup_bp = Blueprint(
+    'signup',
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/signup/static'
+)
 
 
 @signup_bp.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')
-
